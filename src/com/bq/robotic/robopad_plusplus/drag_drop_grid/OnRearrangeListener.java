c4010cc5ce3aa7eacee8e3beb814229c9a1e10a8
Estefania Sarasola Elvira <estefania.sarasola@bq.com>
2014-03-18 11:19:12 +0100

    [JOHNNYFIVE-35] DragDropGrid functionality has been moved to a .jar file in order to be able of be used in other projects.

--- src/com/bq/robotic/robopad_plusplus/drag_drop_grid/OnRearrangeListener.java
+++
@@ -1,8 +0,0 @@
-package com.bq.robotic.robopad_plusplus.drag_drop_grid;
-
-public interface OnRearrangeListener {
-	
-	public abstract void onRearrange(int oldIndex, int newIndex);
-	
-	public abstract void onRearrange(boolean isDraggedDeleted, int draggedDeletedIndex);
-}
